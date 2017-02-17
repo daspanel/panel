7604059023ba73b27dbeaf205050164d0e6660d0
Abner G Jacobsen <abner@apoana.com.br>
2017-02-17 19:17:18 -0200

    Mispelling fix

--- daspanel_web/modules/sites/views.py
+++ daspanel_web/modules/sites/views.py
@@ -9,7 +9,7 @@ Daspanel control panel for web
  :license:   GNU General Public License v3, see LICENSE for more details.
 """
 from __future__ import absolute_import, division, print_function
-from flask import Blueprint, render_template, request, redirect, url_for, flash, current_App
+from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app
 from flask_login import login_required
 import json
 from devourer import GenericAPI, APIMethod, APIError
